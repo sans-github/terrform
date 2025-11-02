48d1efbed6770a7a12b31b589dd04feb4cdf7967
San <San>
2025-11-02 08:32:52 -0800

    Use variables to manage ssh keys

--- ec2-webapp/main.tf
+++ ec2-webapp/main.tf
@@ -3,6 +3,6 @@ resource "aws_instance" "webapp" {
   # https://aws.amazon.com/ec2/pricing/on-demand/?icmpid=docs_console_unmapped
   ami                    = "ami-0c1a6eb95aba250b6"
   vpc_security_group_ids = [aws_security_group.sg_webapp_only_8080.id]
-  key_name               = aws_key_pair.tf_ec2_key_pair.key_name
+  key_name               = aws_key_pair.aws_key_pair.key_name
   user_data = file("setup-app.sh")
 }
--- ec2-webapp/ssh-key.tf
+++ ec2-webapp/ssh-key.tf
@@ -1,24 +1,24 @@
-# Generate an SSH key locally
-resource "tls_private_key" "tf_ec2_key" {
+# Create a private key in PEM and Openssh PEM format
+resource "tls_private_key" "tls_private_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
 # Register the public key with AWS
-resource "aws_key_pair" "tf_ec2_key_pair" {
-  key_name   = "tf_ec2_key"
-  public_key = tls_private_key.tf_ec2_key.public_key_openssh
+resource "aws_key_pair" "aws_key_pair" {
+  key_name   = var.ec2_key_name
+  public_key = tls_private_key.tls_private_key.public_key_openssh
 }
 
-# Output private key for local use
-output "private_key_pem" {
-  value     = tls_private_key.tf_ec2_key.private_key_pem
-  sensitive = true
-}
+# # Output private key for local use
+# output "private_key_pem" {
+#   value     = tls_private_key.tls_private_key.private_key_pem
+#   sensitive = true
+# }
 
 # Save the private key inside ~/.ssh and set 400 permission
-resource "local_file" "tf_ec2_key_file" {
-  content         = tls_private_key.tf_ec2_key.private_key_pem
-  filename        = "${pathexpand("~/.ssh/tf_ec2_key.pem")}"
+resource "local_file" "local_file" {
+  content         = tls_private_key.tls_private_key.private_key_pem
+  filename        = "${pathexpand("~/.ssh/${var.ec2_key_name}.pem")}"
   file_permission = "0400"
 }
